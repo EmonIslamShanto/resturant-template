55bd8ee01090cda102cf8df8ffcbf86e6ac996b1
Emon Islam Shanto <98974290+EmonIslamShanto@users.noreply.github.com>
2024-11-16 19:14:07 +0600

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,11 +30,11 @@ const Navbar = () =>{
             <div className='app__navbar-samllscreen_overlay flex_center slide-bottom'>
         <MdOutlineRestaurantMenu color='#fff' fontSize={24} className='overlay_close' onClick={()=> setToggleMenu(false)}/>
         <ul className='app__navbar-smallscren-links'>
-          <li className='p__opensans'><a href='#home'>Home</a></li>
-          <li className='p__opensans'><a href='#about'>About</a></li>
-          <li className='p__opensans'><a href='#menu'>Menu</a></li>
-          <li className='p__opensans'><a href='#awards'>Awards</a></li>
-          <li className='p__opensans'><a href='#contact'>Contact</a></li>
+          <li className='p__opensans' onClick={()=> setToggleMenu(false)}><a href='#home'>Home</a></li>
+          <li className='p__opensans' onClick={()=> setToggleMenu(false)}><a href='#about'>About</a></li>
+          <li className='p__opensans' onClick={()=> setToggleMenu(false)}><a href='#menu'>Menu</a></li>
+          <li className='p__opensans' onClick={()=> setToggleMenu(false)}><a href='#awards'>Awards</a></li>
+          <li className='p__opensans' onClick={()=> setToggleMenu(false)}><a href='#contact'>Contact</a></li>
         </ul>
       </div>
           )
